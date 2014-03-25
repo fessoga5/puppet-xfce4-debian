347ba5a2816b9e869209b421b853ec28215aad32
root <root@d1stkfactory>
2014-03-25 04:49:23 +0000

    add_screensaver module

--- manifests/screensaver.pp
+++ manifests/screensaver.pp
@@ -2,11 +2,19 @@
 #
 #INSTALL SKYPE ON DESKTOP
 #
-define puppet-xfce4-debian::screensaver () {
+define puppet-xfce4-debian::screensaver ($owner= root) {
 
     #Installl gnome screensaver
     package {"gnome-screensaver": ensure => latest,}
 
     #ADd templates for keyborts layout ctrl+alt+l
     #
+
+    file { "/home/$owner/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml":
+        replace => "yes",
+        owner   => $owner,
+        mode    => 600,
+        content => template('puppet-xfce4-debian/xfce4-keyboard-shortcuts.xml'),
+        require => Package["gnome-screensaver"],
+    }
 }
